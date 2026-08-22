f545863341e97c9d7c30c0d0e6fbac01157a9152
SovereignX001 <209107391+SovereignX001@users.noreply.github.com>
2026-08-22 12:17:59 +0000

    minor changes

--- Lab3/prg3.js
+++ Lab3/prg3.js
@@ -2,18 +2,18 @@ import http from "http"
 
 const server = http.createServer((req,res) => {
     
-    if(http.url=='/'){
-        req.end("<h1>Home Page<h1>");
-    } else if(http.url=='/about'){
-        req.end("<h1>About Us Page<h1>");
-    } else if(http.url=='/product'){
-        req.end(`<h1>Mobile Phone</h1>
+    if(req.url=='/'){
+        res.end("<h1>Home Page<h1>");
+    } else if(req.url=='/about'){
+        res.end("<h1>About Us Page<h1>");
+    } else if(req.url=='/product'){
+        res.end(`<h1>Mobile Phone</h1>
                  <h2>Price: 2000)</h2>
                  <p>Discount: 5%</p>
                  <a href='#'>Buy Now</a>
                  `);
     } else {
-        res.StatusCode=404;
+        res.statusCode=404;
         res.end(`
             <h1>404, Not Found</h1>
             <p>Page not found</p>
