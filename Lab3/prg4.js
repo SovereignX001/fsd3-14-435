d2e6b71c583eabe5bba29d4d374bd0491b48f610
SovereignX001 <209107391+SovereignX001@users.noreply.github.com>
2026-08-25 04:25:41 +0000

    prg 4

---
+++ Lab3/prg4.js
@@ -0,0 +1,11 @@
+import http from "http";
+
+const server = http.createServer((req,res) => {
+    
+  if(req.url=== "/product") res.end(`<h1>Product Page</h1>`);
+  else if (req.url=== "/cart") res.end(`<h1>Cart Page</h1>`);
+  else if (req.url=== "/checkout") res.end(`<h1>Checkout Page</h1>`);
+  else res.end(`<h1>404, Not Found</h1>`);
+});
+
+server.listen(4444, () => console.log("Server is running..."));
