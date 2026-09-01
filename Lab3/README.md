9e1c5b5c2dc208ab24b08fa5cf5923e817e367ca
SovereignX001 <209107391+SovereignX001@users.noreply.github.com>
2026-09-01 04:42:03 +0000

    prg9 and airtag site

--- Lab3/README.md
+++ Lab3/README.md
@@ -13,3 +13,18 @@ npm - node package manager
 403 - forbidden
 404 - not found
 500 - Internal Device Error
+
+# API :
+any api canbe of 4 types
+1 git -> 
+2 post -> 
+
+
+api can be responsed by server with status code and json ping
+api generally start with API/version
+ex - /api/v1/products
+     /api/v2/products/2169
+     /api/v1/users
+     /api/v1/users/mohan
+     /api/v1/product?s=mohan
+by default browser can check only GET request to check other 3 request type we require frontend or 3rd party API tester like postman, thunderclint, ecoapi
---
+++ Lab3/prg9.js
@@ -0,0 +1,45 @@
+import http from "http";
+import { createReadStream } from "fs";
+
+const server = http.createServer((req, res) => {
+
+    if (req.url == "/") {
+        res.setHeader("content-type","text/html");
+        res.statusCode =200;
+        const stream = createReadStream("./pages/airtag.html", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+
+    }
+    else if(req.url === '/mobile'){
+        res.writeHead(200,{"content-type": "text/json"})
+        res.statusCode =200;
+        
+        const stream = createReadStream("./data/products.json", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+    }
+    else if(req.url === '/manual'){
+         res.writeHead(200,{"content-type": "text/plain"})
+       
+        
+        const stream = createReadStream("./data/chatgpt.txt", {
+            encoding: "utf-8"
+        });
+
+        stream.pipe(res);
+    }
+    else {
+        res.statusCode = 404;
+        res.end("Page not found");
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("server on 3000...");
+});
